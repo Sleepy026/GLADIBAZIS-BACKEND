8eeddd32bfc93e56ab7eb77b78420b1bc2acddbc
Sleepy026 <mrbrowngarden@gmail.com>
2020-04-01 11:43:36 +0200

    application props refactored, unused models, fields removed or renamed

--- gladibazis-service/src/main/java/com/gladibazis/gladibazisservice/controller/dto/GladiCredentials.java
+++ gladibazis-service/src/main/java/com/gladibazis/gladibazisservice/controller/dto/GladiCredentials.java
@@ -1,6 +1,5 @@
 package com.gladibazis.gladibazisservice.controller.dto;
 
-import com.gladibazis.gladibazisservice.model.Picture;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -12,5 +11,5 @@ public class GladiCredentials {
     private String name;
     private String color;
     private Integer heigth;
-    private List<Picture> pictures;
+    private List<String> pictures;
 }
